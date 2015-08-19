673e6e780c2567e9d8a031ce2c583edae9b35ecf
Arthur Clemens <arthurclemens@gmail.com>
2015-08-19 02:12:54 +0200

    Fix link to main example page

--- examples/src/app/app/github.es6.js
+++ examples/src/app/app/github.es6.js
@@ -4,7 +4,7 @@ import m from 'mithril';
 
 let content = (opts = {}) => {
 	return m('.github', [
-		!opts.home ? m('a', {href: '/', config: null}, 'All Examples') : null,
+		!opts.home ? m('a', {href: 'index.html', config: null}, 'All Examples') : null,
 		m('hr'),
 		m.trust('mithril-page-slider, Page slider for Mithril for mobile and desktop. Project page on <a href="https://github.com/ArthurClemens/mithril-page-slider">Github</a>.')
 	]);
